8b8ec72feca666c5631d235fa4824e8139556ab7
tranbau <77967379+tranbau@users.noreply.github.com>
2023-02-22 11:33:57 +0700

    Update CRUD_student.py

--- CRUD_student.py
+++ CRUD_student.py
@@ -1,88 +1,76 @@
-from fastapi import FastAPI
-import mysql.connector
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from typing import Union
-# connect to MySQL
-mydb = mysql.connector.connect(
-   host="localhost",
-   user="root",
-   database="mydb"
-)
-mycursor = mydb.cursor()
+from typing import List
+import mysql.connector
 
-# create table student
-mycursor.execute("""
-                CREATE TABLE student(
-                    id integer  auto_increment primary key,
-                    name varchar(255) not null,
-                    address varchar(255),
-                    phone varchar(255)
-                 )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-                 """)
+# kết nối cơ sở dữ liệu
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    database="mydb"
+)
+
+app = FastAPI()
 
 class Student(BaseModel):
     id: int
     name: str
-    address: Union[str, None] = None
-    phone: Union[str, None] = None
-    
-# CRUD APIs
-app = FastAPI()
-# Create
-@app.post("/create_student")
+    address: str = None
+    phone: str = None
+
+mycursor = mydb.cursor()
+
+# CREATE
+@app.post("/students")
 async def create_student(student: Student):
-    mycursor = mydb.cursor()
-    sql = "insert into student(name, address, phone) values (%s, %s, %s)"
-    val = (student.name, student.address, student.phone)
-    mycursor.execute(sql, val)    
+    sql = "INSERT INTO student (id, name, address, phone) VALUES (%s, %s, %s, %s)"
+    val = (student.id, student.name, student.address, student.phone)
+    mycursor.execute(sql, val)
     mydb.commit()
     return {"message": "Student created successfully"}
 
-
-# Read
-@app.get("/read_student")
-async def read_student():
-    mycursor = mydb.cursor()
-    sql = "select * from student"
+# GET
+@app.get("/students", response_model=List[Student])
+async def read_students():
+    sql = "SELECT * FROM student"
     mycursor.execute(sql)
-    myresult = mycursor.fetchall()
-    return myresult
+    results = mycursor.fetchall()
+    students = []
+    for result in results:
+        student = Student(id=result[0], name=result[1], address=result[2], phone=result[3])
+        students.append(student)
+    return students
 
-@app.get("/read_student/{student_id}")
+# GET BY ID
+@app.get("/students/{student_id}", response_model=Student)
 async def read_student(student_id: int):
-    mycursor = mydb.cursor()
-    sql = "select * from student where id = %s"
+    sql = "SELECT * FROM student WHERE id = %s"
     val = (student_id,)
     mycursor.execute(sql, val)
-    myresult = mycursor.fetchall()
-    return myresult
+    result = mycursor.fetchone()
+    if result is None:
+        raise HTTPException(status_code=404, detail="Student not found")
+    student = Student(id=result[0], name=result[1], address=result[2], phone=result[3])
+    return student
 
-# Update
-@app.put("/update_student/{student_id}")
-async def update_student(student_id: int,student: Student):
-    mycursor = mydb.cursor()
-    sql = "update student set name = %s, address = %s, phone = %s where id = %s"
-    val = [student.name, student.address, student.phone, student_id]
+# UPDATE
+@app.put("/students/{student_id}")
+async def update_student(student_id: int, student: Student):
+    sql = "UPDATE student SET name = %s, address = %s, phone = %s WHERE id = %s"
+    val = (student.name, student.address, student.phone, student_id)
     mycursor.execute(sql, val)
     mydb.commit()
+    if mycursor.rowcount == 0:
+        raise HTTPException(status_code=404, detail="Student not found")
     return {"message": "Student updated successfully"}
 
-# Delete
-@app.delete("/delete_student/{student_id}")
+# DELETE
+@app.delete("/students/{student_id}")
 async def delete_student(student_id: int):
-    mycursor = mydb.cursor()
-    sql = "Delete from student where id = %s"
+    sql = "DELETE FROM student WHERE id = %s"
     val = (student_id,)
     mycursor.execute(sql, val)
     mydb.commit()
-    return  {"message": "Student removed successfully"}
-    
-    
-    
-
-        
-    
-    
-
-
-
+    if mycursor.rowcount == 0:
+        raise HTTPException(status_code=404, detail="Student not found")
+    return {"message": "Student removed successfully"}
